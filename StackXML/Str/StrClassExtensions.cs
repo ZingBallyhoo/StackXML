10b0c84b8efc62371e3be8df7d4f08b3c2024d00
zingballyhoo <zingballyhoo@live.co.uk>
2025-05-10 17:11:00 +0100

    str: add helpers for serializing to string + ISpanFormattable

---
+++ StackXML/Str/StrClassExtensions.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace StackXML.Str
+{
+    public static class StrClassExtensions
+    {
+        public static string AsString<T>(this T obj, char separator, IStrFormatter? formatter=null) where T : IStrClass, allows ref struct
+        {
+            var writer = new StrWriter(separator, formatter);
+            try
+            {
+                obj.Serialize(ref writer);
+                return writer.AsSpan().ToString();
+            } finally
+            {
+                writer.Dispose();
+            }
+        }
+        
+        public static bool TryFormat<T>(this T obj, Span<char> destination, out int charsWritten, char separator, IStrFormatter? formatter=null) where T : IStrClass, allows ref struct
+        {
+            var writer = new StrWriter(separator, formatter);
+            try
+            {
+                obj.Serialize(ref writer);
+                var finishedSpan = writer.AsSpan();
+                charsWritten = finishedSpan.Length;
+                return finishedSpan.TryCopyTo(destination);
+            } finally
+            {
+                writer.Dispose();
+            }
+        }
+    }
+}
